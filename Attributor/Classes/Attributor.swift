2cd4311d035f3cb2892fc1bd5c7dc39f0e13bd84
Serge Vysotsky <vysotskiyserhiy@gmail.com>
2018-11-11 11:15:45 +0200

    0.0.1

--- Attributor/Classes/Attributor.swift
+++ Attributor/Classes/Attributor.swift
@@ -8,31 +8,61 @@
 
 import UIKit
 
-class Attributor {
+public class Attributor {
     private let string: String
     private var attributes: [NSRange : [NSAttributedString.Key : Any]] = [:]
     
-    init(string: String) {
+    public init(string: String) {
         self.string = string
     }
     
-    func apply(to label: UILabel) {
+    public var attributed: NSAttributedString {
+        let attributed = NSMutableAttributedString(string: string)
         
+        for (range, attributes) in attributes {
+            attributed.addAttributes(attributes, range: range)
+        }
+        
+        return attributed
+    }
+}
+
+
+// MARK: - Apply
+extension Attributor {
+    public func apply(to object: UILabel) {
+        object.attributedText = attributed
     }
     
-    func adding(attribute value: Any, for key: NSAttributedString.Key, range: NSRange? = nil) -> Attributor {
+    public func apply(to object: UIButton, for state: UIControl.State = .normal) {
+        object.setAttributedTitle(attributed, for: state)
+    }
+    
+    public func apply(to object: UITextView) {
+        object.attributedText = attributed
+    }
+    
+    public func apply(to object: UITextField) {
+        object.attributedText = attributed
+    }
+}
+
+
+// MARK: - Add/Set attributes
+public extension Attributor {
+    public func adding(attribute value: Any, for key: NSAttributedString.Key, range: NSRange? = nil) -> Attributor {
         let unwrappedRange = range ?? NSRange(location: 0, length: string.count)
         attributes[unwrappedRange, default: [:]][key] = value
         return self
     }
     
-    func setting(attribute value: Any, for key: NSAttributedString.Key, range: NSRange? = nil) -> Attributor {
+    public func setting(attribute value: Any, for key: NSAttributedString.Key, range: NSRange? = nil) -> Attributor {
         let unwrappedRange = range ?? NSRange(location: 0, length: string.count)
         attributes[unwrappedRange] = [key : value]
         return self
     }
     
-    func adding(attributes values: [NSAttributedString.Key : Any], range: NSRange? = nil) -> Attributor {
+    public func adding(attributes values: [NSAttributedString.Key : Any], range: NSRange? = nil) -> Attributor {
         let unwrappedRange = range ?? NSRange(location: 0, length: string.count)
         
         for (key, attribute) in values {
@@ -42,19 +72,39 @@ class Attributor {
         return self
     }
     
-    func setting(attributes values: [NSAttributedString.Key : Any], range: NSRange? = nil) -> Attributor {
+    public func setting(attributes values: [NSAttributedString.Key : Any], range: NSRange? = nil) -> Attributor {
         let unwrappedRange = range ?? NSRange(location: 0, length: string.count)
         attributes[unwrappedRange] = values
         return self
     }
+}
+
+
+
+extension Attributor {
+    public func adding(font: UIFont, range: NSRange? = nil) -> Attributor {
+        return adding(attribute: font, for: .font, range: range)
+    }
     
-    var attributed: NSAttributedString {
-        let attributed = NSMutableAttributedString(string: string)
-        
-        for (range, attributes) in attributes {
-            attributed.addAttributes(attributes, range: range)
-        }
-        
-        return attributed
+    public func adding(color: UIColor, range: NSRange? = nil) -> Attributor {
+        return adding(attribute: color, for: .foregroundColor, range: range)
+    }
+    
+    public func adding(kern: CGFloat, range: NSRange? = nil) -> Attributor {
+        return adding(attribute: kern, for: .kern, range: range)
+    }
+    
+    public func adding(lineSpace: CGFloat, range: NSRange? = nil) -> Attributor {
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = lineSpace
+        return adding(attribute: style, for: .paragraphStyle, range: range)
+    }
+}
+
+
+
+extension String {
+    public var attributor: Attributor {
+        return Attributor(string: self)
     }
 }
